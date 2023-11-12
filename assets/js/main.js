37c784077aae3a8711c5fd02065af11930be5383
grasieletinoco <grasieleprt@gmail.com>
2023-11-11 22:58:13 -0300

    ADD Carrossel

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,27 @@ var btnTop = document.querySelector('#button_top');
   window.addEventListener('scroll', btnReveal);
   btnTop.addEventListener('click', TopscrollTo);
 
-  //FINAL BUTTON BACK TO TOP
+//FINAL BUTTON BACK TO TOP
+
+
+//INICIO CARROSSEL
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("main__carrossel__conteudo");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000);
+}
+//FINAL CARROSSEL
